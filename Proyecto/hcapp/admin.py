9a2038b30de6605103df6f27b417feb4c53e8fa0
alejandro <aleglez22@gmail.com>
2017-07-28 10:09:48 -0500

    prohibido eliminar historias, pacientes, pedidos y prohibido modificar algunos de sus campos

--- Proyecto/hcapp/admin.py
+++ Proyecto/hcapp/admin.py
@@ -2,13 +2,45 @@ from django.contrib import admin
 from .models import Paciente, Categoria, TipoEstudio, MedicoSolicitante, Pedido, Radiologo, Subcategoria
 from. import models
 
-admin.site.register(Paciente)
-admin.site.register(models.Pedido)
 admin.site.register(models.Categoria)
 admin.site.register(models.Subcategoria)
 admin.site.register(models.TipoEstudio)
 
-admin.site.register(models.Historia)
+
 admin.site.register(models.Plantilla)
 admin.site.register(models.MedicoSolicitante)
 
+class DeleteNotAllowedModelAdmin(admin.ModelAdmin):
+    # Other stuff here
+    #admin.site.register(models.Historia)
+
+    def get_actions(self, request):
+        #Disable delete
+        actions = super(DeleteNotAllowedModelAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    #solo readonly los campos
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return self.readonly_fields + self.campos_readonly
+        return self.readonly_fields
+
+
+class PedidoModelAdmin(DeleteNotAllowedModelAdmin):
+	campos_readonly=('Paciente', 'Medico','Diagnostico_presuntivo' )
+
+class HistoriaModelAdmin(DeleteNotAllowedModelAdmin):
+	campos_readonly=('Pedido','TipoEstudio','Fecha_creacion')
+
+class PacienteModelAdmin(DeleteNotAllowedModelAdmin):
+	campos_readonly=('Cedula','Nombre','Apellido','Fecha_nacimiento','Fecha_ingreso')
+
+
+
+admin.site.register(models.Historia, HistoriaModelAdmin)
+admin.site.register(models.Paciente, PacienteModelAdmin)
+admin.site.register(models.Pedido, PedidoModelAdmin)
